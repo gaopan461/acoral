863d5d0cc6e15045e1a513ac1e2da001b762fe96
gaopan461 <gaopan461@gmail.com>
2012-08-06 20:32:53 +0800

    增加工具测试

---
+++ tools/ToolTest/ToolTest.h
@@ -0,0 +1,31 @@
+// ToolTest.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CToolTestApp:
+// 有关此类的实现，请参阅 ToolTest.cpp
+//
+
+class CToolTestApp : public CWinApp
+{
+public:
+	CToolTestApp();
+
+// 重写
+	public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CToolTestApp theApp;
